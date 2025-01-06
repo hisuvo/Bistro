b203432c3e51abfd142b85209e09724bd1bd1e8d
suvo <suvodatta72@gamil.com>
2025-01-07 01:14:48 +0600

    user collection  menu collection review collection done

--- bistro-boss-server-with-menu-and-order/index.js
+++ bistro-boss-server-with-menu-and-order/index.js
@@ -8,7 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ClientSession,
+  ObjectId,
+} = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aiyi0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,8 +32,53 @@ async function run() {
     await client.connect();
 
     const database = client.db("bistroDb");
+    const userCollection = database.collection("users");
     const menuCollection = database.collection("menu");
     const reviewCollection = database.collection("reviews");
+    const cartsCollection = database.collection("carts");
+
+    // get user info to db
+    app.get("/users", async (req, res) => {
+      const reslut = await userCollection.find().toArray();
+      res.send(reslut);
+    });
+
+    // update user role from server in api
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const reslut = await userCollection.updateOne(filter, updateDoc);
+      res.send(reslut);
+    });
+
+    // Delete user infor database
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const reslut = await userCollection.deleteOne(filter);
+      res.send(reslut);
+    });
+
+    // ganaret user collection db
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // insert email if user dosent exists:
+      // I can do this many way (1.email unique, 2.upsert, 3.simple checking)
+      const email = user.email;
+      const query = { email: email };
+      const isExistUser = await userCollection.findOne(query);
+      if (isExistUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      } else {
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      }
+    });
 
     app.get("/menus", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -37,6 +87,39 @@ async function run() {
 
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // find carts collection from server
+    // ----------- first way -------------
+    app.get("/cart/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // find carts collection from server
+    // ----------- second way -----------
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // carts collections in db
+    app.post("/carts", async (req, res) => {
+      const data = req.body;
+      const result = await cartsCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // delete carts iteams
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
 
